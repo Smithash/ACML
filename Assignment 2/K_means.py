9629b346a53ac17870853b7a66d0586413186737
Ashlea <smithashlea@gmail.com>
2024-05-09 12:01:00 +0200

    Setup of clusters, dataset and centres

---
+++ Assignment 2/K_means.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+#Implementing K-means
+
+
+
+#2. Dataset
+data = np.array([[0.22, 0.33], [0.45, 0.76], [0.73, 0.39], [0.25, 0.35], 
+                [0.51, 0.69], [0.69, 0.42], [0.41, 0.49], [0.15,0.29],
+                [0.81, 0.32], [0.50, 0.88], [0.23, 0.31], [0.77, 0.30],
+                [0.56, 0.75], [0.11, 0.38], [0.81, 0.33], [0.59, 0.77],
+                [0.10, 0.89], [0.55, 0.09], [0.75, 0.35], [0.44, 0.55]])
+
+
+#4. Run k-means Algorithm using hard coded dataset and starting eith cluster centres
+    #5. halt when centres have converged
+    
+
+    
+
+#6. Mean squared error
+
+    
+cluster_centers =[]
+def main():
+    #1. Number of clusters is 3
+    K = 3
+    
+    #3. Read in from standard input a list of 6 numbers
+    for i in range(3):
+        x = float(input())
+        y = float(input())
+        cluster_centers.append((x,y))
+    
+    
+    
+    
+if __name__ == "__main__":
+    main()
