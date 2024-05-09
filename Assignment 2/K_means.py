1695c2dd4a32296b8399ec5837c08aacade721bf
Ashlea <smithashlea@gmail.com>
2024-05-09 12:22:06 +0200

    K-means algorithm and error calculation

--- Assignment 2/K_means.py
+++ Assignment 2/K_means.py
@@ -14,25 +14,53 @@ data = np.array([[0.22, 0.33], [0.45, 0.76], [0.73, 0.39], [0.25, 0.35],
 
 #4. Run k-means Algorithm using hard coded dataset and starting eith cluster centres
     #5. halt when centres have converged
+def K_Means(data, cluster_centers, K):
+    initial_centres = cluster_centers
     
+    while(True):
+        
+        #Compute the minimum distance for each datapoint
+        distances = np.linalg.norm(data[:, np.newaxis] - cluster_centers, axis=-1)
+        #assign x to its cluster where the distance is a minimum 
+        assignments = np.argmin(distances, axis=1)
+
+        #Update the centres
+        new_centers = np.array([data[assignments == i].mean(axis=0) for i in range(K)])
+        
+        if np.all(cluster_centers == new_centers):
+            break
+        
+        cluster_centers = new_centers
+        
+    return cluster_centers, assignments
+        
 
     
 
-#6. Mean squared error
+#6. sum of squares error
+def sum_of_squares_error(data, cluster_centres, assignments):
+    sse = 0
+    for i, centroid in enumerate(cluster_centres):
+        cluster_points = data[np.where(assignments == i)]  # Get data points assigned to the current centroid
+        distances = np.linalg.norm(cluster_points - centroid, axis=1)  # Calculate distance to centroid
+        sse += np.sum(distances ** 2)  # Sum of squared distances
+    return np.round(sse,4)
 
-    
-cluster_centers =[]
 def main():
     #1. Number of clusters is 3
     K = 3
     
     #3. Read in from standard input a list of 6 numbers
-    for i in range(3):
+    cluster_centers =[]
+    for _ in range(3):
         x = float(input())
         y = float(input())
         cluster_centers.append((x,y))
     
-    
+    #4. K-Means
+    final_cluster_centers, assignments = K_Means(data, cluster_centers, K)
+    output = sum_of_squares_error(data, final_cluster_centers, assignments)
+    print(output)
     
     
 if __name__ == "__main__":
